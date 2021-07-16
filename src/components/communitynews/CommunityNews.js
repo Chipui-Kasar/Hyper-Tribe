cc00a448b7bd8c7ae2960a70c25192b0d48a23fc
chipui-kasar <chipuikasar@gmail.com>
2021-07-16 16:31:08 +0530

    Commit

--- src/components/communitynews/CommunityNews.js
+++ src/components/communitynews/CommunityNews.js
@@ -1,121 +1,99 @@
 import React from "react";
 import "./CommunityNews.css";
 import communityNews from "../../images/CommunityNews.png";
+import { CommunityNewsData } from "../../AllData";
 
 function CommunityNews(props) {
   return (
     <>
       <div className="col-md-7">
-        <div className="whiteBox CommunityNewsSliderBox">
-          <div className="whiteBox__heading">{props.title}</div>
-          <a href="/communitynews" className="viewAll">
-            View All
-          </a>
-          <div className="CommunityNewsSlider">
-            <div
-              id="CommunityNewsSlider"
-              className="carousel slide"
-              data-ride="carousel"
-            >
-              <ol className="carousel-indicators">
-                <li
-                  data-target="#CommunityNewsSlider"
-                  data-slide-to="0"
-                  className="active"
-                ></li>
-                <li data-target="#CommunityNewsSlider" data-slide-to="1"></li>
-                <li data-target="#CommunityNewsSlider" data-slide-to="2"></li>
-                <li data-target="#CommunityNewsSlider" data-slide-to="3"></li>
-              </ol>
-              <div className="carousel-inner">
-                <div className="item active">
-                  <div
-                    className="CommunityNewsSlider__img"
-                    style={{ background: `url(${communityNews})` }}
-                  ></div>
-                  <h3 className="heading">
-                    Praesent sagittis eu ante vel tincidunt. Integer nulla nibh,
-                    fringilla sit amet purus a, lobortis eleifend quam.
-                  </h3>
-                  <div className="dateTimeBox">
-                    <div className="dateInfo">06 Sep 2019</div>
-                    <div className="timeInfo">06 Sep 2019</div>
+        {CommunityNewsData.map(data => {
+          return (
+            <div className="whiteBox CommunityNewsSliderBox">
+              <div className="whiteBox__heading">{props.title}</div>
+              <a href="/communitynews" className="viewAll">
+                View All
+              </a>
+              <div className="CommunityNewsSlider">
+                <div
+                  id="CommunityNewsSlider"
+                  className="carousel slide"
+                  data-ride="carousel"
+                >
+                  <ol className="carousel-indicators">
+                    <li
+                      data-target="#CommunityNewsSlider"
+                      data-slide-to="0"
+                      className="active"
+                    ></li>
+                    <li
+                      data-target="#CommunityNewsSlider"
+                      data-slide-to="1"
+                    ></li>
+                    <li
+                      data-target="#CommunityNewsSlider"
+                      data-slide-to="2"
+                    ></li>
+                    <li
+                      data-target="#CommunityNewsSlider"
+                      data-slide-to="3"
+                    ></li>
+                  </ol>
+                  <div className="carousel-inner">
+                    <div className="item active">
+                      <div
+                        className="CommunityNewsSlider__img"
+                        style={{ background: `url(${data.src})` }}
+                      ></div>
+                      <h3 className="heading">{data.heading}</h3>
+                      <div className="dateTimeBox">
+                        <div className="dateInfo">{data.date}</div>
+                        <div className="timeInfo">{data.time}</div>
+                      </div>
+                      <p>{data.content}</p>
+                    </div>
+                    <div className="item">
+                      <div
+                        className="CommunityNewsSlider__img"
+                        style={{ background: `url(${data.src})` }}
+                      ></div>
+                      <h3 className="heading">{data.heading}</h3>
+                      <div className="dateTimeBox">
+                        <div className="dateInfo">{data.date}</div>
+                        <div className="timeInfo">{data.time}</div>
+                      </div>
+                      <p>{data.content}</p>
+                    </div>
+                    <div className="item">
+                      <div
+                        className="CommunityNewsSlider__img"
+                        style={{ background: `url(${data.src})` }}
+                      ></div>
+                      <h3 className="heading">{data.heading}</h3>
+                      <div className="dateTimeBox">
+                        <div className="dateInfo">{data.date}</div>
+                        <div className="timeInfo">{data.time}</div>
+                      </div>
+                      <p>{data.content}</p>
+                    </div>
+                    <div className="item">
+                      <div
+                        className="CommunityNewsSlider__img"
+                        style={{ background: `url(${data.src})` }}
+                      ></div>
+                      <h3 className="heading">{data.heading}</h3>
+                      <div className="dateTimeBox">
+                        <div className="dateInfo">{data.date}</div>
+                        <div className="timeInfo">{data.time}</div>
+                      </div>
+                      <p>{data.content}</p>
+                    </div>
                   </div>
-                  <p>
-                    Praesent vitae nisi at urna imperdiet cursus quis a sem.
-                    Quisque semper dui et lorem faucibus, sed varius mi
-                    sagittis. Sed bibendum nulla placerat pharetra vehicula.
-                    Curabitur dolor est, sagittis a diam ut, consectetur dapibus
-                    nunc.
-                  </p>
-                </div>
-                <div className="item">
-                  <div
-                    className="CommunityNewsSlider__img"
-                    style={{ background: `url(${communityNews})` }}
-                  ></div>
-                  <h3 className="heading">
-                    Praesent sagittis eu ante vel tincidunt. Integer nulla nibh,
-                    fringilla sit amet purus a, lobortis eleifend quam.
-                  </h3>
-                  <div className="dateTimeBox">
-                    <div className="dateInfo">06 Sep 2019</div>
-                    <div className="timeInfo">06 Sep 2019</div>
-                  </div>
-                  <p>
-                    Praesent vitae nisi at urna imperdiet cursus quis a sem.
-                    Quisque semper dui et lorem faucibus, sed varius mi
-                    sagittis. Sed bibendum nulla placerat pharetra vehicula.
-                    Curabitur dolor est, sagittis a diam ut, consectetur dapibus
-                    nunc.
-                  </p>
-                </div>
-                <div className="item">
-                  <div
-                    className="CommunityNewsSlider__img"
-                    style={{ background: `url(${communityNews})` }}
-                  ></div>
-                  <h3 className="heading">
-                    Praesent sagittis eu ante vel tincidunt. Integer nulla nibh,
-                    fringilla sit amet purus a, lobortis eleifend quam.
-                  </h3>
-                  <div className="dateTimeBox">
-                    <div className="dateInfo">06 Sep 2019</div>
-                    <div className="timeInfo">06 Sep 2019</div>
-                  </div>
-                  <p>
-                    Praesent vitae nisi at urna imperdiet cursus quis a sem.
-                    Quisque semper dui et lorem faucibus, sed varius mi
-                    sagittis. Sed bibendum nulla placerat pharetra vehicula.
-                    Curabitur dolor est, sagittis a diam ut, consectetur dapibus
-                    nunc.
-                  </p>
-                </div>
-                <div className="item">
-                  <div
-                    className="CommunityNewsSlider__img"
-                    style={{ background: `url(${communityNews})` }}
-                  ></div>
-                  <h3 className="heading">
-                    Praesent sagittis eu ante vel tincidunt. Integer nulla nibh,
-                    fringilla sit amet purus a, lobortis eleifend quam.
-                  </h3>
-                  <div className="dateTimeBox">
-                    <div className="dateInfo">06 Sep 2019</div>
-                    <div className="timeInfo">06 Sep 2019</div>
-                  </div>
-                  <p>
-                    Praesent vitae nisi at urna imperdiet cursus quis a sem.
-                    Quisque semper dui et lorem faucibus, sed varius mi
-                    sagittis. Sed bibendum nulla placerat pharetra vehicula.
-                    Curabitur dolor est, sagittis a diam ut, consectetur dapibus
-                    nunc.
-                  </p>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </>
   );
